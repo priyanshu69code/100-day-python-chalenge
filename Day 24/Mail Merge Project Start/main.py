bef633220c0c69433eec8a2502b62f83017e4375
Priyanshu Kumar <jjjjyotijha10@gmail.com>
2023-03-19 16:45:02 +0530

    Completed the mail merge project in one go........

--- Day 24/Mail Merge Project Start/main.py
+++ Day 24/Mail Merge Project Start/main.py
@@ -1,3 +1,13 @@
-with open("./Input/Letters/starting_letter.txt") as letters:
+PLACEHOLDER = "[name]"
+
+with open("Day 24\Mail Merge Project Start\Input\Letters\starting_letter.txt") as letters:
     letter = letters.read()
-    print(letter)
+
+with open(r"Day 24\Mail Merge Project Start\Input\Names\invited_names.txt") as names:
+    list_name = names.readlines()
+
+for name in list_name:
+    modified_name = name.strip()
+    new_letter = letter.replace(PLACEHOLDER, modified_name)
+    with open(f'Day 24\Mail Merge Project Start\Output\ReadyToSend\letter_for_{modified_name}.txt', mode="w") as completd_letter:
+        completd_letter.write(new_letter)
